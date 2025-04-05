8447589560bb66d13fda9a5cdf7c1b56294c8dea
OroszPeter <petiorosz4@gmail.com>
2025-04-05 13:46:58 +0200

    init

--- svelte.config.js
+++ svelte.config.js
@@ -3,18 +3,7 @@ import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// Használjuk a Netlify adaptert
-		adapter: adapter({
-			// Edge functions használata a jobb teljesítményért
-			edge: true,
-			
-			// Split the app into smaller chunks
-			split: false,
-			external: [],
-			esbuild: {
-				target: 'node16'
-			}
-		})
+		adapter: adapter()
 	}
 };
 
